9c1fdbe966ed4549ae0423f29e3fbb9770b64850
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2023-02-13 11:35:57 -0400

    React hook form to  MovieFilter (Genre combo)

--- src/components/MovieFilter/index.tsx
+++ src/components/MovieFilter/index.tsx
@@ -1,13 +1,60 @@
+import { useEffect, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import Select from 'react-select';
+import { Genre } from 'types/genre';
+
+import { requestBackend } from 'util/requests';
 import './styles.css';
 
+type GenreFilterData = {
+  genre: Genre;
+}
+
 const MovieFilter = () => {
+  const [selectGenres, setSelectGenres] = useState<Genre[]>();
+
+  const {
+    register,
+    handleSubmit,
+    control
+  } = useForm<GenreFilterData>();
+
+  const onSubmit = (formData: GenreFilterData) => {
+    console.log("ENVIOU", formData);
+  };
+
+  useEffect(() => {
+    requestBackend({
+      url: '/genres',
+      withCredentials: true
+    })
+    .then(response => {
+      setSelectGenres(response.data);
+    })
+  }, []);
+
   return (
     <div className="base-card  movie-filter-card">
-      <form action="" className="movie-filter-form">
+      <form onSubmit={ handleSubmit(onSubmit) } className="movie-filter-form">
         <div className="movie-filter-genre-container">
-          <select name="" id="">
+          <Controller 
+            name="genre"
+            control={ control }
+            render={({ field }) => (
+              <Select 
+                { ...field }
+                options={ selectGenres }
+                isClearable
+                classNamePrefix="genre-select"
+                getOptionLabel={ (genre: Genre) => genre.name }
+                getOptionValue={ (genre: Genre) => String(genre.id) }
+              />
+            )}
+          />
+          
+          {/* <select name="" id="">
             <option value="">Terror</option>
-          </select>
+          </select> */}
         </div>
       </form>
     </div>
